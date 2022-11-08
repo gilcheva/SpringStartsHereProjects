cf289f17b2687a585e829127708412237910f03d
lambova <gilcheva@abv.bg>
2022-11-08 09:30:07 +0200

    SFS07 –v.3.2- @Autowired over constructor and @Component

---
+++ SFS07/src/main/java/beans/Person.java
@@ -0,0 +1,30 @@
+package beans;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    private String name = "Bobi";
+
+    private final Parrot parrot;
+
+    @Autowired
+    public Person(Parrot parrot) {
+        this.parrot = parrot;
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
