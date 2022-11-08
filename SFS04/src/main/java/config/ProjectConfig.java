88b8cc4866a6cb2902d413d141c97906cfb10853
lambova <gilcheva@abv.bg>
2022-11-08 09:23:54 +0200

    SFS04 – Wire 2 beans - v.1 - @Bean direct method call

---
+++ SFS04/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,26 @@
+package config;
+
+import model.Parrot;
+import model.Person;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    Parrot parrot(){
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    Person person(Parrot parrot) {
+        Person p = new Person();
+        p.setName("Vladi");
+        p.setParrot(parrot);
+        return p;
+    }
+
+}
