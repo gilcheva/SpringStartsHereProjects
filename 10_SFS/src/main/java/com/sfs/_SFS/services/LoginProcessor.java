cb0e0eeab69642a3dcfa356c4d12972ecd62d3a0
lambova <gilcheva@abv.bg>
2022-11-15 12:10:09 +0200

    10_SFS – Login – @ApplicationScope bean

---
+++ 10_SFS/src/main/java/com/sfs/_SFS/services/LoginProcessor.java
@@ -0,0 +1,39 @@
+package com.sfs._SFS.services;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Getter
+@Setter
+@Component
+@RequestScope
+public class LoginProcessor {
+
+    private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
+
+    private String username;
+    private String password;
+
+    public LoginProcessor(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
+        this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
+    }
+
+    public boolean login() {
+        loginCountService.increment();
+
+        String username = this.getUsername();
+        String password = this.getPassword();
+
+        boolean loginResult = false;
+
+        if ("natalie".equals(username) && "password".equals(password)) {
+            loginResult = true;
+            loggedUserManagementService.setUsername(username);
+        }
+        return loginResult;
+    }
+}
