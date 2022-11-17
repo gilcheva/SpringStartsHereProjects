0ba9d6764b2fd1070d659190c51d789d955b9586
lambova <gilcheva@abv.bg>
2022-11-17 15:33:37 +0200

    12_SFS – @RestController

---
+++ 12_SFS/src/main/java/com/sfs/_SFS/config/ProjectConfig.java
@@ -0,0 +1,9 @@
+package com.sfs._SFS.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"controllers"})
+public class ProjectConfig {
+}
