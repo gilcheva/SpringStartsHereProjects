d187c223ed58a64729b945b47a390ee2c4dc8782
lambova <gilcheva@abv.bg>
2022-11-17 16:59:01 +0200

    14_SFS - ResponseEntity – custom headers and response status

---
+++ 14_SFS/src/main/java/com/sfs/_SFS/controllers/CountryController.java
@@ -0,0 +1,22 @@
+package com.sfs._SFS.controllers;
+
+import com.sfs._SFS.dto.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/france")
+    public ResponseEntity<Country> france() {
+        Country c = Country.of("France", 67);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("continent", "Europe")
+                .header("capital","Paris")
+                .header("favorite_food","chees and wine")
+                .body(c);
+    }
+}
