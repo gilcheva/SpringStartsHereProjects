61f63059d9eb6e1d4ce922196d818772f75ee6ae
lambova <gilcheva@abv.bg>
2022-11-17 15:34:25 +0200

    13_SFS - sending objects as a response body - Country

---
+++ 13_SFS/src/main/java/com/sfs/_SFS/dto/Country.java
@@ -0,0 +1,20 @@
+package com.sfs._SFS.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Country {
+
+    private String name;
+    private int population;
+
+    public static Country of(String name, int population) {
+        Country country = new Country();
+        country.setName(name);
+        country.setPopulation(population);
+        return country;
+    }
+
+}
