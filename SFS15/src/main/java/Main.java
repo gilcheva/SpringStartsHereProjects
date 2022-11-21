c3b7544e73c8542928cc0fe9cbb58e7646aefe7f
lambova <gilcheva@abv.bg>
2022-11-21 12:13:16 +0200

    SFS_15 – CommentApp –  using field DI

---
+++ SFS15/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import config.ProjectConfiguration;
+import model.Comment;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
+
+        var comment = new Comment();
+        comment.setAuthor("Gal");
+        comment.setText("testing comment");
+
+        var commentService = context.getBean(CommentService.class);
+        commentService.publishComment(comment);
+    }
+}
