cef4d4ad8feeece9f6663be880f3a9551801cb86
lambova <gilcheva@abv.bg>
2022-11-21 12:06:15 +0200

    16_SFS - aspect custom exception handling

---
+++ 16_SFS/src/main/java/com/sfs/_SFS/exceptions/ErrorDetails.java
@@ -0,0 +1,11 @@
+package com.sfs._SFS.exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorDetails {
+
+    private String message;
+}
---
+++ 16_SFS/src/main/java/com/sfs/_SFS/model/PaymentDetails.java
@@ -0,0 +1,11 @@
+package com.sfs._SFS.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PaymentDetails {
+
+    private double amount;
+}
