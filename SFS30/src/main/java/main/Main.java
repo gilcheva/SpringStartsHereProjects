b8320057b929df7d3f97d83d3b6c8d2897791f6e
lambova <gilcheva@abv.bg>
2022-12-29 12:11:20 +0200

    Add Logger as an aspect

---
+++ SFS30/src/main/java/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import config.ProjectConfig;
+import models.Comment;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        var service = c.getBean(CommentService.class);
+
+        Comment comment = new Comment();
+        comment.setText("Demo comment");
+        comment.setAuthor("Stanimir");
+
+        service.publishComment(comment);
+    }
+}
