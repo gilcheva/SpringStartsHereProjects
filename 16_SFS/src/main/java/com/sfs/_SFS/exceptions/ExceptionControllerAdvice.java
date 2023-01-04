cdfedd6e991104ccc575ec9db032940b67ce415d
lambova <gilcheva@abv.bg>
2023-01-04 12:49:55 +0200

    16_SFS - Add @ExceptionHandler(NotEnoughMoneyException.class)

--- 16_SFS/src/main/java/com/sfs/_SFS/exceptions/ExceptionControllerAdvice.java
+++ 16_SFS/src/main/java/com/sfs/_SFS/exceptions/ExceptionControllerAdvice.java
@@ -1,11 +1,13 @@
 package com.sfs._SFS.exceptions;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
+    @ExceptionHandler(NotEnoughMoneyException.class)
     public ResponseEntity<ErrorDetails> exceptionNotEnoughMoneyHandler() {
         ErrorDetails errorDetails = new ErrorDetails();
         errorDetails.setMessage("Not enough money to make the payment.");
