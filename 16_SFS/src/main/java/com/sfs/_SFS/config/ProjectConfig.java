bc00da0990a147671b78700b2ef40ff86e027add
lambova <gilcheva@abv.bg>
2023-01-04 12:53:35 +0200

    16_SFS - Remove unnecessary @Bean in @Configuration

--- 16_SFS/src/main/java/com/sfs/_SFS/config/ProjectConfig.java
+++ 16_SFS/src/main/java/com/sfs/_SFS/config/ProjectConfig.java
@@ -1,20 +1,11 @@
 package com.sfs._SFS.config;
 
-import com.sfs._SFS.exceptions.ExceptionControllerAdvice;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-//@EnableAspectJAutoProxy
 @Configuration
 @ComponentScan(basePackages = {"services", "exceptions"})
 public class ProjectConfig {
-
-    @Bean
-    public ExceptionControllerAdvice aspect() {
-        return new ExceptionControllerAdvice();
-    }
 
 }
 
