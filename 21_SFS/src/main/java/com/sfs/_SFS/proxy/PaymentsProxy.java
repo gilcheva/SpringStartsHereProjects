8cb8ed5d974bab4560d8c4918b93c8b34e37419d
lambova <gilcheva@abv.bg>
2023-01-09 14:47:10 +0200

    21_SFS - WebClient - reactive way to call Payment service

---
+++ 21_SFS/src/main/java/com/sfs/_SFS/proxy/PaymentsProxy.java
@@ -0,0 +1,36 @@
+package com.sfs._SFS.proxy;
+
+import com.sfs._SFS.model.Payment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Component
+public class PaymentsProxy {
+
+    private final WebClient webClient;
+
+    @Value("${name.service.url}")
+    private String paymentsServiceUrl;
+
+    public PaymentsProxy(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Mono<Payment> createPayment(String requestId, Payment payment) {
+        return webClient.post()
+                .uri(paymentsServiceUrl +"/payment")
+                .header("requestId", requestId)
+                .body(Mono.just(payment), Payment.class)
+                .retrieve()
+                .bodyToMono(Payment.class);
+    }
+}
