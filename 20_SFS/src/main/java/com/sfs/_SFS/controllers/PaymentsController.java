fe847f13e758c4b3de153347bc16061d14d0d754
lambova <gilcheva@abv.bg>
2023-01-09 14:56:15 +0200

    20_SFS - remove unnecessary lines

--- 20_SFS/src/main/java/com/sfs/_SFS/controllers/PaymentsController.java
+++ 20_SFS/src/main/java/com/sfs/_SFS/controllers/PaymentsController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
 @RestController
 public class PaymentsController {
 
@@ -19,7 +17,6 @@ public class PaymentsController {
 
     @PostMapping("/payment")
     public Payment createPayment(@RequestBody Payment payment) {
-        String requestId = UUID.randomUUID().toString();
         return paymentsProxy.createPayment(payment);
     }
 }
--- 20_SFS/src/main/java/com/sfs/_SFS/proxy/PaymentsProxy.java
+++ 20_SFS/src/main/java/com/sfs/_SFS/proxy/PaymentsProxy.java
@@ -1,7 +1,6 @@
 package com.sfs._SFS.proxy;
 
 import com.sfs._SFS.model.Payment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
