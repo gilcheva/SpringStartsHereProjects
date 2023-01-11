aa3c9fe787310a52a14a370aac6b4cfefc415ade
lambova <gilcheva@abv.bg>
2023-01-11 11:27:33 +0200

    23_SFS - add MySQL database, use JdbcTemplate - methods addPurchase() and findAll()

---
+++ 23_SFS/src/main/java/com/example/SFS/controllers/PurchaseController.java
@@ -0,0 +1,32 @@
+package com.example.SFS.controllers;
+
+import com.example.SFS.models.Purchase;
+import com.example.SFS.repositories.PurchaseRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+
+    public final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases(){
+        return purchaseRepository.findAllPurchases();
+    }
+}
